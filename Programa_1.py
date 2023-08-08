2644cfbbaa4605f98ffd139fdfa928995f8c089a
Agustin G <141699827+agustinnicolasgallardorios@users.noreply.github.com>
2023-08-08 01:52:02 -0300

    Add files via upload

---
+++ Programa_1.py
@@ -0,0 +1,14 @@
+print("Bienvenido al conversor de pesos a monedas extranjera")
+
+calculadora = float(input("ingresa el valor de peso, que desea convertir "))
+
+valor_dol = 244
+valor_euro = 263
+valor_libra = 307
+
+
+print("el valor de peso a dolar convertido sera igual a ", calculadora * valor_dol)
+print("el valor de peso a euro convertido sera igual a ", calculadora * valor_euro)
+print("el valor de peso a libra convertido sera igual a ", calculadora * valor_libra)
+
+calculadora = float(input("Gracias por usar el programa"))
